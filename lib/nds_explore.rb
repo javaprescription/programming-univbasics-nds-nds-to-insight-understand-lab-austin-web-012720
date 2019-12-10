127bfa56a13d171f3f2506622b48741b4c998ae6
Austin Scott <austinscott4@gmail.com>
2019-12-10 21:18:33 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ def print_first_directors_movie_titles
   counter = 0
   while counter < directors_database[0][:movies].length do
     if directors_database[0][:movies][counter] == :title
-      puts directors_database[0][:movies][counter]
+      directors_database[0][:movies][counter]
     end
     counter += 1
   end
